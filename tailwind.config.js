0da8718da693ffb54143a76fe03c776bc3bca6cd
auutstudio <github@findauut.com>
2021-10-13 12:41:05 -0700

    add list of css classes as safely used

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
   purge: {
     mode: "all",
     content: ["./**/*.html"],
+    safelist: [
+      'img1','img2','img3','img4','img5','img6','img7','img8',
+      'img9','img10','img11','img12','img13','img14','img15',
+      'img16','img17','img18','img19','img20',
+      '::-webkit-scrollbar-track',
+      '::-webkit-scrollbar-thumb',
+    ],
     options: {
       whitelist: [],
     },
